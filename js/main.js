bb373de963bd8e93f57628bd278c1a18ce5a4a46
Roman <roman.sav15@gmail.com>
2021-08-17 19:07:47 +0300

    final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-
+  $('.mfp-iframe').magnificPopup({
+    type: 'image',
+    // other options
+    closeMarkup: '<button class="mfp-close"/></button>'
+  });
+  $('.images-container').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery:{
+      enabled:true
+    },
+    closeMarkup: '<button class="mfp-close"/></button>'
+    // other options
+  });
+  var rellax = new Rellax('.rellax', {
+    horizontal: true,
+    wrapper: 'main',
+    vertical: false,
+    speed: -5,
+    center: true,
+    //Disable vertical Parallax Scrolling     vertical:false
+  });
 });
